4af69e66da71317948caaaa68a3008e6f399fd84
Tsurova Tamara Andreevna <101869480+TamaraTsu@users.noreply.github.com>
2023-12-22 14:32:01 +0700

    Update script.sh

--- unix-2/script.sh
+++ unix-2/script.sh
@@ -4,7 +4,7 @@ directory="/data"
 max_files=999
 lock_file="$directory/synchronization_lock"
 
-# Function to generate a random container identifier
+# Генерация случайного идентификатора контейнера
 generate_random_identifier() {
     seed=$(date +%s)
     random_string=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 64 | head -n 1)
@@ -12,17 +12,16 @@ generate_random_identifier() {
     echo "$container_identifier"
 }
 
-# Function to find the next available file name in the directory
+# Определение незанятого имени
 find_next_available_name() {  
     for i in $(seq -w 1 "$max_files"); do
         filename="$(printf "%03d" "$i")" # Format as "001," "002," etc.
-        # Check if the file does not exist in the directory
         if [ ! -e "$directory/$filename" ]; then
             echo "$filename"
-            return # Return the first available file name
+            return
         fi
     done
-    # If all file names are occupied
+    # Если нет свободных
     echo "No available file names"
 }
 
@@ -33,16 +32,12 @@ while true; do
         next_filename=$(find_next_available_name)
 
         if [ ! -e "$directory/$next_filename" ]; then
-            # Generate a random container identifier
             container_identifier=$(generate_random_identifier)
             content="$next_filename : $container_identifier"
-
             echo "$content" >"$directory/$next_filename"
         fi
     ) 200>"$flock_file"
 
-    sleep 1
-    # Add an additional sleep to delay before next step
     sleep 1
 
     (
